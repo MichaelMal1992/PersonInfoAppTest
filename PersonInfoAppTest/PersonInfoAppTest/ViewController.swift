1f58c08fe144303472b1bef4fbaa9ba1cfa1d9da
MichaelMal1992 <>
2021-09-29 14:43:51 +0300

    Initial project

---
+++ PersonInfoAppTest/PersonInfoAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonInfoAppTest
+//
+//  Created by Admin on 29.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
